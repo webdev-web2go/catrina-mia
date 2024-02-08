10630f2406ae3b23828c9737c35ccd07e4a6db76
webdev-web2go <desarrollos.web2go@gmail.com>
2024-02-08 13:07:44 -0600

    Add Clerk for auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
-import "@/styles/globals.css";
-
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
+import "@/styles/globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,8 +19,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`font-sans ${inter.variable}`}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }

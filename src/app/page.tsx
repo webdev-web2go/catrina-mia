4a07d4c528a56dd267b0e41dfe9296925d6df971
webdev-web2go <desarrollos.web2go@gmail.com>
2024-02-12 13:59:27 -0600

    Add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-export default function HomePage() {
+export default async function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white"></main>
+    <main className="flex h-screen flex-col items-center justify-center gap-6"></main>
   );
 }

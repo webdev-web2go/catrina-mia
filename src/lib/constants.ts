53672c1d863c2d52d684d9db12f755edc716f7ef
webdev-web2go <desarrollos.web2go@gmail.com>
2024-03-14 19:13:39 -0600

    Add categories map

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,44 @@
+export const CATEGORIES = [
+  {
+    id: "bodas",
+    label: "Bodas",
+  },
+  {
+    id: "diaDeMmuertos",
+    label: "Día de muertos",
+  },
+  {
+    id: "navidad",
+    label: "Navidad",
+  },
+  {
+    id: "fantasia",
+    label: "Fantasía",
+  },
+  {
+    id: "teatro",
+    label: "Teatro",
+  },
+  {
+    id: "pasarela",
+    label: "Pasarela",
+  },
+  {
+    id: "eventoSocial",
+    label: "Evento social",
+  },
+  {
+    id: "bebes",
+    label: "Bebés",
+  },
+  {
+    id: "8M",
+    label: "8M",
+  },
+];
+
 export const STRIPE_WEBHOOK_EVENTS = {
   CHARGE_SUCCEEDED: "charge.succeeded",
 } as const;
 
-export const PRODUCTS_PER_PAGE = 8;
+export const PRODUCTS_PER_PAGE = 8 as const;
